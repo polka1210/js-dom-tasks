f83baf8b6e294beb7a753d03e8efbd1beb79d184
polina12 <polkriv@gmail.com>
2026-06-10 10:56:37 +0300

    d

--- solutions/11/src/application.js
+++ solutions/11/src/application.js
@@ -2,58 +2,45 @@ import 'whatwg-fetch';
 
 export default () => {
   // BEGIN
- const capitalInput = document.getElementById('capital');
-  const countryInput = document.getElementById('country');
+const inputs = document.querySelectorAll('[data-autocomplete]');
   
-  const capitalList = document.querySelector('[data-autocomplete-name="capital"]');
-  const countryList = document.querySelector('[data-autocomplete-name="country"]');
-  
-  if (capitalInput && capitalList) {
-    capitalInput.oninput = function() {
-      const val = this.value;
-      if (val === '') {
-        capitalList.innerHTML = '<li>Nothing</li>';
+  for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+    const url = input.getAttribute('data-autocomplete');
+    const name = input.getAttribute('data-autocomplete-name');
+    const list = document.querySelector('[data-autocomplete-name="' + name + '"]');
+    
+    if (!list) continue;
+    
+    input.addEventListener('input', async function(e) {
+      const value = e.target.value;
+      
+      if (value === '') {
+        list.innerHTML = '<li>Nothing</li>';
         return;
       }
-      fetch('/capitals.json?search=' + val)
-        .then(function(r) { return r.json(); })
-        .then(function(d) {
-          capitalList.innerHTML = '';
-          if (d.length === 0) {
-            capitalList.innerHTML = '<li>Nothing</li>';
-          } else {
-            for (var i = 0; i < d.length; i++) {
-              var li = document.createElement('li');
-              li.innerText = d[i];
-              capitalList.appendChild(li);
-            }
+      
+      const fullUrl = window.location.origin + url + '?search=' + encodeURIComponent(value);
+      
+      try {
+        const response = await fetch(fullUrl);
+        const data = await response.json();
+        
+        list.innerHTML = '';
+        
+        if (data.length === 0) {
+          list.innerHTML = '<li>Nothing</li>';
+        } else {
+          for (let j = 0; j < data.length; j++) {
+            const li = document.createElement('li');
+            li.textContent = data[j];
+            list.appendChild(li);
           }
-        });
-    };
-  }
-  
-  if (countryInput && countryList) {
-    countryInput.oninput = function() {
-      const val = this.value;
-      if (val === '') {
-        countryList.innerHTML = '<li>Nothing</li>';
-        return;
+        }
+      } catch (error) {
+        list.innerHTML = '<li>Nothing</li>';
       }
-      fetch('/countries.json?search=' + val)
-        .then(function(r) { return r.json(); })
-        .then(function(d) {
-          countryList.innerHTML = '';
-          if (d.length === 0) {
-            countryList.innerHTML = '<li>Nothing</li>';
-          } else {
-            for (var i = 0; i < d.length; i++) {
-              var li = document.createElement('li');
-              li.innerText = d[i];
-              countryList.appendChild(li);
-            }
-          }
-        });
-    };
+    });
   }
   // END
 };
